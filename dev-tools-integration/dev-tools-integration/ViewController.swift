fa527f6e87ba36ce809b19378407c4b199cdb04b
Misra Serveshwar <serveshwarmishra@gmail.com>
2019-04-17 12:40:30 +0530

    project setup

---
+++ dev-tools-integration/dev-tools-integration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-tools-integration
+//
+//  Created by Misra Serveshwar on 17/04/19.
+//  Copyright © 2019 Misra Serveshwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dev-tools-integration/dev-tools-integrationTests/dev_tools_integrationTests.swift
@@ -0,0 +1,34 @@
+//
+//  dev_tools_integrationTests.swift
+//  dev-tools-integrationTests
+//
+//  Created by Misra Serveshwar on 17/04/19.
+//  Copyright © 2019 Misra Serveshwar. All rights reserved.
+//
+
+import XCTest
+@testable import dev_tools_integration
+
+class dev_tools_integrationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
